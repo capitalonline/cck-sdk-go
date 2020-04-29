d043183cecf178a8be4684472ced6f08f416dce8
jizhi.sui@capitalonline.net <jizhi.sui@capitalonline.net>
2020-04-29 17:32:33 +0800

    Modify types.go

--- pkg/cck/types.go
+++ pkg/cck/types.go
@@ -5,9 +5,62 @@ type BaseResponse struct {
 	Message string `json:"msg"`
 }
 
+type DescribeNasInstanceSlice struct {
+	ID 			string`json:"id"`
+	Name 		string`json:"name"`
+	SiteID 		string`json:"site_id"`
+	ClusterID 	string`json:"cluster_id"`
+	DiskType 	string`json:"disk_type"`
+	Iops 		string`json:"iops"`
+	Size 		string`json:"size"`
+	Status 		string`json:"status"`
+	CreateTime	string`json:"create_time"`
+	StorageVmId string`json:"storage_vm_id"`
+	MountPoint  string`json:"Mount_point"`
+	BackupDiskMountPath string`json:"backup_disk_mount_path"`
+	StatusStr   string`json:"status_str"`
+	Usage 		string`json:"usage"`
+	UsageRate	string`json:"usage_rate"`
+}
+
+type DescribeNasInstancesResponse struct {
+	BaseResponse
+	Data struct {
+		NasInfo []DescribeNasInstanceSlice `json:"nas_info"`
+		Total int `json:"total"`
+	} `json:"data"`
+}
+
+type CreateNasResponse struct {
+	BaseResponse
+	Data struct {
+		TaskId string `json:"task_id"`
+	} `json:"data"`
+}
+
+type ResizeNasResponse struct {
+	BaseResponse
+}
+
+type DeleteNasResponse struct {
+	BaseResponse
+}
+
 type MountNasResponse struct {
 	BaseResponse
 	Data struct {
 		TaskId string `json:"task_id"`
 	} `json:"data"`
 }
+
+type UnMountNasResponse struct {
+	BaseResponse
+	Data struct {
+		TaskId string `json:"task_id"`
+	} `json:"data"`
+}
+
+
+
+
+
