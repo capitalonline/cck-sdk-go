5997a0684427f09897c2609a484bfbcd1983c7aa
junqiang0718 <junqiang.wang@capitalonline.net>
2023-07-30 13:11:15 +0800

    delete clusterName

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -19,7 +19,6 @@ const (
 	signatureVersion       = "1.0"
 	signatureMethod        = "HMAC-SHA1"
 	timeStampFormat        = "2006-01-02T15:04:05Z"
-	clusterName            = "CLUSTER_NAME"
 )
 
 const (
@@ -80,10 +79,6 @@ func init() {
 		if os.Getenv(overseaFlag) == "True" {
 			APIHost = defaultApiHostOversea
 		}
-	}
-
-	if ClusterName == "" {
-		ClusterName = os.Getenv(clusterName)
 	}
 }
 
